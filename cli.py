b3e88f6db26d29d66b661844af75492abb5cf425
Ovation <your-real-email@example.com>
2025-08-10 19:21:17 -0600

    made to conform with PEP 8

--- cli.py
+++ cli.py
@@ -1,41 +1,39 @@
-import functions
 import time
+
+import functions
 
 now = time.strftime("%b %d, %Y %H:%M:%S")
 print("It is", now)
 
 while True:
-    user_action = input("Type add, show, edit, complete or exit: ")
-    user_action = user_action.strip()
+    user_action = input("Type add, show, edit, complete or exit: ").strip()
 
     if user_action.startswith("add"):
         todo = user_action[4:]
 
         todos = functions.get_todos()
-
         todos.append(todo + "\n")
-
         functions.write_todos(todos)
 
     elif user_action.startswith("show"):
         todos = functions.get_todos()
 
-        for index, item in enumerate(todos):
-            item = item.strip("\n")
-            row = f"{index + 1}-{item}"
+        for index, item in enumerate(todos, start=1):
+            item = item.rstrip("\n")
+            row = f"{index} - {item}"
             print(row)
+
     elif user_action.startswith("edit"):
         try:
             number = int(user_action[5:])
             print(number)
 
-            number = number - 1
+            number -= 1
 
             todos = functions.get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + "\n"
-
             functions.write_todos(todos)
         except ValueError:
             print("Your command is not valid.")
@@ -47,9 +45,8 @@ while True:
 
             todos = functions.get_todos()
             index = number - 1
-            todo_to_remove = todos[index].strip("\n")
+            todo_to_remove = todos[index].rstrip("\n")
             todos.pop(index)
-
             functions.write_todos(todos)
 
             message = f"Todo {todo_to_remove} was removed from the list."
--- web.py
+++ web.py
@@ -1,4 +1,5 @@
 import streamlit as st
+
 import functions
 
 todos = functions.get_todos()
@@ -14,7 +15,6 @@ st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
-
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
     if checkbox:
@@ -24,5 +24,8 @@ for index, todo in enumerate(todos):
         st.rerun()
 
 st.text_input(
-    label="", placeholder="Add new todo...", on_change=add_todo, key="new_todo"
+    label="",
+    placeholder="Add new todo...",
+    on_change=add_todo,
+    key="new_todo",
 )
